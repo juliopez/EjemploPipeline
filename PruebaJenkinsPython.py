15b5b911481d97ed50fdaf00bc4f53284431ea01
juliopez <30697797+juliopez@users.noreply.github.com>
2020-10-25 21:02:10 +0000

    Segundo Commit
    Subiendo Pipeline

--- PruebaJenkinsPython.py
+++
@@ -1,8 +0,0 @@
-def sum(a, b):
-    for n in (a, b):
-        if not isinstance(n, int) and not isinstance(n, float):
-            raise TypeError
-    return a + b
-
-
-
--- test_PruebaJenkinsPython.py
+++
@@ -1,9 +0,0 @@
-
-from PruebaJenkinsPython import *
-import unittest
-class TestMyModule(unittest.TestCase):
-    
-    def test_sum(self):
-        self.assertEqual(sum(5, 7), 12)
-if __name__ == "__main__":
-    unittest.main()
